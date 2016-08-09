ce315d24bfa9205bdc507ef9edc3f5eec400ac11
Johnny Lee Othon <jleeothon@gmail.com>
2016-08-09 21:54:47 +0200

    Added installation of ruby, python, postgres

--- cookbooks/alfiecookbook/recipes/default.rb
+++ cookbooks/alfiecookbook/recipes/default.rb
@@ -21,8 +21,45 @@ apt_package 'python3.4-venv'
 # TODO pip3 install jinja2
 # TODO fix language
 
-
 template "#{Dir.home('vagrant')}/.bashrc" do
   source "bashrc"
   owner "vagrant"
+end
+
+script "install-ruby2.2" do
+  not_if do
+    `ruby -v`.strip.between? "ruby 2.2", "ruby2.3"
+  end
+  interpreter "bash"
+  code <<-END
+    apt-add-repository -y ppa:brightbox/ruby-ng
+    apt-get update -y
+    apt-get install -y ruby2.2
+  END
+end
+
+script "install-jinja2" do
+  not_if do
+    `pip3 freeze`.include? "Jinja2==2.8"
+  end
+  interpreter "bash"
+  code <<-END
+    pip3 install -Iv Jinja2==2.8
+  END
+end
+
+template "/etc/apt/sources.list.d/postgresql.list" do
+  source "postgresql.list"
+end
+
+script "install-postgresql" do
+  not_if do
+    `psql --version || true`.strip == "psql (PostgreSQL) 9.5.3"
+  end
+  interpreter "bash"
+  code <<-END
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+    apt-get update -y
+    apt-get install -y postgresql-9.5 # You may need --force-yes
+  END
 end
--- cookbooks/alfiecookbook/recipes/repos.rb
+++ cookbooks/alfiecookbook/recipes/repos.rb
@@ -12,15 +12,15 @@ directory "#{HOME}/cortoproject" do
 end
 
 corto_repos = [
-  ["cortoproject", "corto"],
-  ["cortoproject", "c-binding"],
-  ["cortoproject", "xml"],
-  ["cortoproject", "corto-language"],
-  ["cortoproject", "io"],
-  ["cortoproject", "cortodoc"],
-  ["cortoproject", "test"],
-  ["cortoproject", "json"],
-  ["cortoproject", "web"],
+  ["cortoproject", "corto", "0044adadf8e504128ff98a475e4a9e0e21844a71"],
+  ["cortoproject", "c-binding", "0de7d81901ccd77a18f54f4742853f1f2ef8e22a"],
+  ["cortoproject", "xml", "85bc8aa8f75c6a33bd47862b6df8efe51264703a"],
+  ["cortoproject", "corto-language", "d857d7d0e43136a074ae3036dc546548bdc1612c"],
+  ["cortoproject", "io", "9bec7a136f82f7a9ade8e7e17d5a9bdf5dda226c"],
+  ["cortoproject", "cortodoc", "a9f9f38e96908e23ab0fa8f051eaf02847b21136"],
+  ["cortoproject", "test", "6904a482b259d599f15c6228303ca00a568ee258"],
+  ["cortoproject", "json", "0045469acf4102ea1ba2e3ed7b60eacb95d4a9c0"],
+  ["cortoproject", "web", "0e7c37e0785df2d99717a2c1fb3fab5f59f0c93a"],
   # Figure out how to clone private repos later
   # ["jleeothon", "alfie"],
   # ["jleeothon", "alfieapp"],
@@ -31,6 +31,7 @@ corto_repos.each do |repo|
     repository "https://github.com/#{repo[0]}/#{repo[1]}"
     action :checkout
     user "vagrant"
+    revision repo[2]
   end
 end
 
